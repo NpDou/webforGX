5ce96da9dff86ea59f659e2bbcb753284461bcbb
NpDou <aa_npdou@163.com>
2022-07-17 18:49:18 +0800

    删除打印信息

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -52,7 +52,8 @@ export default {
             tab:'',
             type:'',
             content:'',
-            detail:{}
+            detail:{},
+            userInfo:{}
         }
     },
     computed:{
